85865eed69072941ec210958b317b85ee45f9865
EdVassie <e.vassie@btinternet.com>
2018-07-02 14:25:54 +0100

    Delete SQLFineWorkstation.bat

--- SQL2008R2/SQLFineWorkstation.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-REM Copyright FineBuild Team © 2008 - 2016.  Distributed under Ms-Pl License
-REM
-CALL "SqlFineBuild.bat" %*     /Type:Workstation             /IAcceptLicenseTerms          ^
- /SAPWD:"UseAL0ngPa55phrase!"  /GroupDBA:"GBGGDBAS01" 
