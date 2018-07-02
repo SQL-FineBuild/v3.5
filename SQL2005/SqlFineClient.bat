c0f18eeed227508f557e0524bb35bfcfef1e5112
EdVassie <e.vassie@btinternet.com>
2018-07-02 14:07:48 +0100

    Add files via upload

---
+++ SQL2005/SqlFineClient.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM Copyright FineBuild Team © 2008 - 2016.  Distributed under Ms-Pl License
+REM
+CALL "SqlFineBuild.bat" %*     /Type:Client                  /IAcceptLicenseTerms          ^
+ /GroupDBA:"GBGGDBAS01"        /GroupDBANonSA:"GBGGDBAN01"
