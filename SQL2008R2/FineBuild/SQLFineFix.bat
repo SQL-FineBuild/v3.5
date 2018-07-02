9ea99551ba7050e7eaa710258d4d711a69738671
EdVassie <e.vassie@btinternet.com>
2018-07-02 21:26:10 +0100

    Add files via upload

---
+++ SQL2008R2/FineBuild/SQLFineFix.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM Copyright FineBuild Team © 2008 - 2016.  Distributed under Ms-Pl License
+REM
+CALL "SqlFineBuild.bat" %*     /Type:Fix                     /IAcceptLicenseTerms          ^
+ /SetupSP:Yes /SetupSPCU:Yes   /SetupSPCUSNAC:Yes /SetupBOL:Yes     ^
+ /SPLevel:SP2 /SPCULevel:CU1
