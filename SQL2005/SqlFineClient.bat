907e5b8f68153208387d6e72615deee260d794de
EdVassie <e.vassie@btinternet.com>
2018-07-02 14:17:38 +0100

    Delete SqlFineClient.bat

--- SQL2005/SqlFineClient.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-REM Copyright FineBuild Team © 2008 - 2016.  Distributed under Ms-Pl License
-REM
-CALL "SqlFineBuild.bat" %*     /Type:Client                  /IAcceptLicenseTerms          ^
- /GroupDBA:"GBGGDBAS01"        /GroupDBANonSA:"GBGGDBAN01"
