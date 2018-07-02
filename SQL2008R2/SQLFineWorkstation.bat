20c5355573ead8dab87e145615dc579945608bc0
EdVassie <e.vassie@btinternet.com>
2018-07-02 14:09:59 +0100

    Add files via upload

---
+++ SQL2008R2/SQLFineWorkstation.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM Copyright FineBuild Team © 2008 - 2016.  Distributed under Ms-Pl License
+REM
+CALL "SqlFineBuild.bat" %*     /Type:Workstation             /IAcceptLicenseTerms          ^
+ /SAPWD:"UseAL0ngPa55phrase!"  /GroupDBA:"GBGGDBAS01" 
