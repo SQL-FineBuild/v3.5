31653117d04db8ef112af76d738dd94b169cac63
EdVassie <e.vassie@btinternet.com>
2021-06-21 10:27:08 +0100

    Delete SQLFineRebuild.bat

--- SQL2019/FineBuild/SQLFineRebuild.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-REM Copyright FineBuild Team © 2020.  Distributed under Ms-Pl License
-REM
-REM /Type:Rebuild allows re-running a portion of SQL FineBuild after the initial build has completed successfully
-REM The example in this script refreshes the Generic Maintenance procedures
-REM
-CALL "SQLFineBuild.bat" %*     /Type:Rebuild                 /IAcceptLicenseTerms          ^
- /SetupGenMaint:Yes                                                                        ^
- /Restart:5EF
